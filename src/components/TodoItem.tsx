7aeb04bf909a0bc586ef851c8e5a35b0eeef1ba1
danstorek <dan.storek@gmail.com>
2021-01-26 12:42:09 +0100

    error in deleteitem

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { TodoItem as TodoItemType } from '../types';
 import { H1 } from './atomic/H1';
+import './todoItem.css';
 
-type TodoItemProps = TodoItemType;
+type TodoItemProps = TodoItemType & 
+{deleteItem: (id: number) => void};
 
 export const TodoItem: React.FC<TodoItemProps> = ({id, title, description}) => {
     return (
@@ -13,6 +15,7 @@ export const TodoItem: React.FC<TodoItemProps> = ({id, title, description}) => {
         <div>
             {description}
         </div>
+        <button onClick={()=>deleteItem(id)}>Delete</button>
     </div>
 
         {/* <pre style={{textAlign: "left"}}>{JSON.stringify(props, null, 2)}</pre> */}
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { TodoItem as TodoItemType } from '../types';
 import { TodoItem } from './TodoItem';
 
@@ -6,12 +6,18 @@ type TodoListProps = {
     items: TodoItemType[];
 }
 
-export const TodoList: React.FC<TodoListProps> = ({items}) => {
+export const TodoList: React.FC<TodoListProps> = ({items:itemsProps}) => {
+    const[items, setItems] = useState(itemsProps);
+    const deleteItem = (id: number) => {
+        setItems(items.filter(x=>x.id!==id));
+    }
+
     return (
     <div>
         {items.map((item) => (
-            <TodoItem key={item.id} {...item} />
+            <TodoItem key={item.id} {...item} deleteItem={deleteItem}/>
         ))}
+
         {/* <pre style={{textAlign: "left"}}>{JSON.stringify(props, null, 2)}</pre> */}
     </div>
     );
